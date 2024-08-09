95f2f44dfdd1c7e13f5bcb2bea3c0cbc7e342a67
rindeal <dev.rindeal@gmail.com>
2024-08-09 21:08:01 +0200

    Create fun.sh

---
+++ fun.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+## Usage:
+##     core.setFailedAndExit  "No 'repo' input provided."
+##     core.setFailedAndExit  "Invalid 'repo' input."  "Check 'repo' format: '%s'" "${REPO}"
+core.setFailedAndExit() { printf "::error title=$1::${2-$1}\n" "${@:3}" ; exit 1 ; }
