a4709aacbc1697167f39ac181cb84551e965afab
rindeal <dev.rindeal@gmail.com>
2024-08-09 22:16:00 +0200

    Update fun.sh

--- fun.sh
+++ fun.sh
@@ -4,3 +4,5 @@
 ##     core.setFailedAndExit  "No 'repo' input provided."
 ##     core.setFailedAndExit  "Invalid 'repo' input."  "Check 'repo' format: '%s'" "${REPO}"
 core.setFailedAndExit() { printf "::error title=$1::${2-$1}\n" "${@:3}" ; exit 1 ; }
+
+export -f  core.setFailedAndExit
